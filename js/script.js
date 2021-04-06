057529ae4bc479083e0135ac80dfe1d059262725
mromann2 <mromann@inbox.ru>
2021-04-06 20:15:06 +0300

    dz 4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 /* Задание на урок:
 
-1) Первую часть задания повторить по уроку
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
@@ -29,51 +31,54 @@ let personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
-}
-
-
-function rememberMyFilms(){
-    for (let i = 1; i <=2; i++){
-        const  movie  = prompt('Один из последних просмотренных фильмов?'),
-               score = prompt('На сколько оцените его?');
-         if (movie === null || score === null || movie === '' || score === '' ||  movie.length > 50){
-             i--
-         }else {
-             personalMovieDB.movies[movie] = score
+    rememberMyFilms: () => {
+        for (let i = 1; i <=2; i++){
+            const  movie  = prompt('Один из последних просмотренных фильмов?'),
+                   score = prompt('На сколько оцените его?');
+             if (movie === null || score === null || movie === '' || score === '' ||  movie.length > 50){
+                 i--
+             }else {
+                 personalMovieDB.movies[movie] = score
+             }
          }
-     }
+    },
+    writeYourGenres: () => {
+        for(let i = 1; i <= 3; i++){
+            const ganre = prompt(`Ваш любимый жанр под номером ${i}`)
+                if (ganre ==='' || ganre === null){
+                    i--
+                }else {
+                    personalMovieDB.genres.push(ganre)
+                }  
+        }
+        personalMovieDB.genres.forEach( (current, index) => console.log(`Любимый жанр #${index + 1} - это ${current}`) )
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10){
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count > 30){
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: () => {
+        if (!personalMovieDB.privat){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: () => {
+        personalMovieDB.privat = !personalMovieDB.privat;
+    },
 }
 
-rememberMyFilms();
-
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres.push( prompt(`Ваш любимый жанр под номером ${i}`) )
-    }
-}
-
-writeYourGenres()
-
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10){
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count > 30){
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-
-detectPersonalLevel()
-
-function showMyDB(){
-    if (!personalMovieDB.privat){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB()
+personalMovieDB.rememberMyFilms()
+personalMovieDB.detectPersonalLevel()
+personalMovieDB.writeYourGenres()
+personalMovieDB.toggleVisibleMyDB()
+personalMovieDB.showMyDB()
 
 
 
