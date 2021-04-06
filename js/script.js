839d57fe41d3291893883775252c77c0d5fb2af1
mromann2 <mromann@inbox.ru>
2021-04-06 10:58:55 +0300

    not nesessery

--- js/script.js
+++ js/script.js
@@ -36,11 +36,11 @@ for (let i = 1; i <=2; i++){
 
 if (personalMovieDB.count < 10){
     console.log("Просмотрено довольно мало фильмов");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
     console.log("Вы классический зритель");
 } else if (personalMovieDB.count > 30){
     console.log("Вы киноман");
-} else{
+} else {
     console.log("Произошла ошибка");
 }
 
